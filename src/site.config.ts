13ada40b1f78fdcf6f33fd9ee7291ccb6fc2f866
yamhochi <yam.hoechee@gmail.com>
2024-10-28 23:41:45 +1100

    remove chris williams

--- src/site.config.ts
+++ src/site.config.ts
@@ -25,7 +25,8 @@ export const siteConfig: SiteConfig = {
 	title: "Andie Yam",
 	webmentions: {
 		// Webmention.io API endpoint. Get your own here: https://webmention.io/, and follow this blog post: https://astro-cactus.chriswilliams.dev/posts/webmentions/
-		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+		// link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+		link: "",
 	},
 };
 
